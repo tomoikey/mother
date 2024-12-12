96fcb90719cc76e11cd6fc0976a0d73e6385a5df
tomoikey <55743826+tomoikey@users.noreply.github.com>
2024-12-12 20:12:30 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod text_box;
 
 use crate::text_box::TextBox;
 use ab_glyph::{FontRef, PxScale};
+use clap::{Parser, ValueEnum};
 use image::codecs::gif::{GifEncoder, Repeat};
 use image::{Delay, Frame, GenericImageView, Rgba};
 use imageproc::drawing::draw_text_mut;
 use std::fs::File;
+use std::path::Path;
 
 const WIDTH: u32 = 960;
 const HEIGHT: u32 = 256;
@@ -42,20 +44,40 @@ fn init_image() -> image::ImageBuffer<Rgba<u8>, Vec<u8>> {
     image_buffer
 }
 
-fn main() {
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    text: String,
+    #[clap(short, long)]
+    output: String,
+    #[clap(short, long, default_value = "8")]
+    speed: u32,
+    #[clap(short, long, default_value = "plain")]
+    theme: Theme,
+}
+
+#[derive(Clone, Copy, ValueEnum)]
+enum Theme {
+    Plain,
+    Banana,
+}
+
+fn main() -> anyhow::Result<()> {
+    let Args {
+        text,
+        output,
+        speed,
+        theme,
+    } = Args::parse();
+
     let image_buffer = init_image();
 
-    let font = FontRef::try_from_slice(FONT_BYTES).expect("Error constructing Font");
-    let mut text = TextBox::<20>::new(
-        "はるかさん Linux のちょうしは\nどうですか？",
-    );
+    let font = FontRef::try_from_slice(FONT_BYTES)?;
+    let mut text = TextBox::<20>::new(text);
 
-    let file = File::create("dest/output.gif").expect("Error creating file");
-    let mut encoder = GifEncoder::new(file);
+    let mut encoder = GifEncoder::new(File::create(Path::new(output.as_str()))?);
 
-    encoder
-        .set_repeat(Repeat::Infinite)
-        .expect("Error setting repeat");
+    encoder.set_repeat(Repeat::Infinite)?;
 
     while let Some(lines) = text.next() {
         let mut image_buffer = image_buffer.clone();
@@ -75,7 +97,9 @@ fn main() {
             HEIGHT as f32 - 60.0,
         );
 
-        let frame = Frame::from_parts(image_buffer, 0, 0, Delay::from_numer_denom_ms(8, 100));
-        encoder.encode_frame(frame).expect("Error encoding frame");
+        let frame = Frame::from_parts(image_buffer, 0, 0, Delay::from_numer_denom_ms(speed, 100));
+        encoder.encode_frame(frame)?;
     }
+
+    Ok(())
 }
